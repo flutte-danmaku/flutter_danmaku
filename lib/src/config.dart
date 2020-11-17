be4505438393769ab6806904ee618e895de4bed6
chenyu <a13868300128a@126.com>
2020-11-17 20:58:21 +0800

    修复底部弹幕位置低了一轨道的问题

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter_danmaku/flutter_danmaku.dart';
 import 'package:flutter_danmaku/src/flutter_danmaku_manager.dart';
 
 class FlutterDanmakuConfig {
